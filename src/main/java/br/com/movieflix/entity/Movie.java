593b833832123bcd74b52700d4cf2fa1d83ade82
Iago Zancanaro <iagozancanaro@gmail.com>
2025-08-20 20:06:56 -0300

    feat: criada entidade movie, assim como seu controller, service e repository. Relacionamento entre tabelas e migrations

--- src/main/java/br/com/movieflix/entity/Movie.java
+++ src/main/java/br/com/movieflix/entity/Movie.java
@@ -2,6 +2,12 @@ package br.com.movieflix.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Builder
 @Entity
@@ -16,13 +22,35 @@ public class Movie {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    @Column(nullable = false)
+    private String title;
 
     private String description;
 
-    @ManyToOne
-    private Category category;
+    @Column(name = "release_date")
+    private LocalDate releaseDate;
 
-    @ManyToOne
-    private Streaming streaming;
+    private double rating;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @ManyToMany
+    @JoinTable(name = "movie_category",
+            joinColumns = @JoinColumn(name = "movie_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private List<Category> categories;
+
+    @ManyToMany
+    @JoinTable(name = "movie_streaming",
+            joinColumns = @JoinColumn(name = "movie_id"),
+            inverseJoinColumns = @JoinColumn(name = "streaming_id")
+    )
+    private List<Streaming> streamings;
 }
