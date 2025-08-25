665dc842ac15c502fc41c28a814438bb0c8264cb
Iago Zancanaro <iagozancanaro@gmail.com>
2025-08-25 17:23:59 -0300

    feat(movie): criado repository, service, controller e endpoints

--- src/main/java/br/com/movieflix/controller/MovieController.java
+++ src/main/java/br/com/movieflix/controller/MovieController.java
@@ -1,9 +1,17 @@
 package br.com.movieflix.controller;
 
+import br.com.movieflix.controller.request.MovieRequest;
+import br.com.movieflix.controller.response.MovieResponse;
+import br.com.movieflix.entity.Movie;
+import br.com.movieflix.mapper.MovieMapper;
 import br.com.movieflix.service.MovieService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/movieflix/movie")
@@ -12,5 +20,56 @@ public class MovieController {
 
     private final MovieService movieService;
 
+    @PostMapping
+    public ResponseEntity<MovieResponse> save(@RequestBody MovieRequest request) {
+        Movie savedMovie = movieService.saveMovie(MovieMapper.toMovie(request));
+        return ResponseEntity.ok(MovieMapper.toMovieResponse(savedMovie));
+
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MovieResponse>> findAllMovies() {
+        List<MovieResponse> movies = movieService.findAll()
+                .stream()
+                .map(MovieMapper::toMovieResponse)
+                .toList();
+
+        return ResponseEntity.ok(movies);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MovieResponse> findMovieById(@PathVariable Long id) {
+        return movieService.findById(id)
+                .map(movie -> ResponseEntity.ok(MovieMapper.toMovieResponse(movie)))
+                .orElse(ResponseEntity.notFound().build());
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MovieResponse> updateMovie(@PathVariable Long id, @RequestBody MovieRequest request) {
+        return movieService.update(id, MovieMapper.toMovie(request))
+                .map(movie -> ResponseEntity.ok(MovieMapper.toMovieResponse(movie)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<MovieResponse>> findByCategory(@RequestParam Long category) {
+        return ResponseEntity.ok(movieService.findByCategory(category)
+                .stream()
+                .map(MovieMapper::toMovieResponse)
+                .toList());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        Optional<Movie> optMovie = movieService.findById(id);
+        if (optMovie.isPresent()) {
+            movieService.delete(id);
+            return ResponseEntity.noContent().build();
+        }
+        return  ResponseEntity.notFound().build();
+    }
+
 
 }
+
