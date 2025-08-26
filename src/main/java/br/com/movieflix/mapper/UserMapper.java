890f1138741e53725349c7fd8268ab6b0dc9d5ca
Iago Zancanaro <iagozancanaro@gmail.com>
2025-08-26 20:30:28 -0300

    refactor: alterações no auth/user e migrations

--- src/main/java/br/com/movieflix/mapper/UserMapper.java
+++ src/main/java/br/com/movieflix/mapper/UserMapper.java
@@ -20,6 +20,7 @@ public class UserMapper {
     public static UserResponse toUserResponse(User user) {
         return UserResponse
                 .builder()
+                .id(user.getId())
                 .name(user.getName())
                 .email(user.getEmail())
                 .build();
